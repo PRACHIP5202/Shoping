eec93cf6f25703a19f493e9c57c2df0f659c8099
Prachi P <prachi.p10305@gmail.com>
2025-04-12 14:27:31 +0530

    Show duplicates in cart

--- e-comm/src/context/CartContext.jsx
+++ e-comm/src/context/CartContext.jsx
@@ -4,8 +4,17 @@ export const cartContext = createContext();
 
 export function CartProvider({children}){
   const [cartItems, setCartItems]= useState([]);
+
   const addToCart=(product)=>{
-    setCartItems((prevItems)=>[...prevItems, product]);
+    const isAlreadyInCart = cartItems.some((item) => item.id === product.id);
+    if(!isAlreadyInCart){
+      setCartItems((prevItems)=>[...prevItems, product]);
+    }
+    else{
+      alert("Item already in cart");
+    }
+
+    
   }
 
   const removeFromCart=(productId)=>{
